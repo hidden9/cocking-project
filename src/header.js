e605cf1d1da328fa0167741699904232c3ef31a2
hidden9 <39252771+hidden9@users.noreply.github.com>
2021-06-29 20:57:57 +0430

    update header.js

--- src/header.js
+++ src/header.js
@@ -1,23 +1,30 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 
-function Header(){
+function Header() {
 
-    
 
-        return(
-            <nav class="navbar navbar-light bg-dark">
+
+    return (
+        <nav class="navbar navbar-light bg-dark">
             <a class="navbar-brand text-light">Cooking page</a>
             <form class="form-inline">
-                <button class="btn btn-outline-success my-1 mr-sm-3 " type="submit">Signup</button>
-                <button class="btn btn-primary my-1 my-sm-0" type="submit" >Login</button>
+                <Link to="/signup" className="btn btn-outline-success my-1 mr-sm-3">SignUp</Link>
+                <Link to="/login" className="btn btn-primary my-1 my-sm-0">Login</Link>
             </form>
         </nav>
-        );
-    }
+    );
+}
 
 
 
 
 
 export default Header;
+
+
+
+
+
+
