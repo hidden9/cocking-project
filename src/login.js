b05e45a386ad98c1b1df6411379f41901def0814
hidden9 <39252771+hidden9@users.noreply.github.com>
2021-07-01 23:48:38 +0430

    df

--- src/login.js
+++ src/login.js
@@ -1,97 +1,92 @@
 import React, { useState } from 'react';
 import { Route, useHistory } from 'react-router-dom';
-import signup from './signup'
-import Header from "./header"
+import Login from './login'
+
+import { button } from 'reactstrap'
 
 
 
 
 
+export default function SignUp() {
 
-
-
-
-function Login() {
 
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [isloggedin, setIsloggedin] = useState(false);
+    const [password_confimation, setPassword_confimation] = useState("");
 
-
-
-
-
+    const [name, setName] = useState("");
 
 
     let sendData = () => {
-        axios.post('localhost:8000/api/login', {email , password})
-            .then(res => {
-                console.log('Data send');
-                setIsloggedin(true);
-                
-        
-        }
-            
-            )
+        axios.post('localhost:8000/api/register', { email,name, password, password_confimation })
+            .then(res => console.log('Data send'))
             .catch(err => console.log(err.data))
     }
 
 
+
     function validateFormFields() {
-        return email.length > 0 && password.length > 0;
+        return email.length > 0 && password.length > 0 && name.length > 0;
     }
     function handleSubmit(event) {
         event.preventDefault();
     }
 
+
+
+
+
     let history = useHistory();
 
-
-
-
-
-
-
     return (
-
         <div className="body bg-dark" style={{ paddingBottom: "20%" }}>
-
-            <Header />
-
-            <div className='container my-5 ' style={{ padding: "10%" }}>
-
-
+            <div className='container  ' style={{ padding: "10%" }}>
+                <h4 className="text-center text-light">Sign up</h4>
+                <br></br>
                 <div className='row h-100 justify-content-center align-items-center'>
-                    <div className='col-10 col-md-8 col-lg-6'>
-                        <form className="form-example" onSubmit={handleSubmit} action="" method="post">
-                            <h4 className="text-center text-light">Login</h4>
-                            <div className='form-group '>
-                                <br></br>
-                                <input type="text" className="form-control username my-2" id="email" placeholder="Email..." name="email" onChange={e => setEmail(e.target.value)}></input>
+
+
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-row ">
+                            <div className="form-group col-lg-12 my-3">
+
+                                <input type="name" className="form-control" id="inputname4" placeholder="Name" onChange={e => setName(e.target.value)}></input>
                             </div>
-                            <div className='form-group input-group'>
-                                <input type="password" className="form-control password" id="password" placeholder="Password..." name="password" onChange={e => setPassword(e.target.value)}></input>
+                            <div class="form-group col-lg-12 my-3">
+
+                                <input type="email" className="form-control" id="inputEmail4" placeholder="Email" onChange={e => setEmail(e.target.value)}></input>
                             </div>
-                            <div class="d-grid gap-2 my-3">
-                                <button type="button" className="btn btn-primary  btn-block" disabled={!validateFormFields()} onChange={sendData} onClick={() => { history.push("/welcome") }}>Login</button>
+                            <div class="form-group col-lg-12 my-3">
+
+                                <input type="password" className="form-control" id="inputPassword4" placeholder="Password" ></input>
                             </div>
+                            <div class="form-group col-lg-12 my-3">
+
+                                <input type="password" className="form-control" id="inputPassword4" placeholder="Confirm Password"  onChange={e => setPassword(e.target.value)}  ></input>
+                            </div>
+                            <br></br>
+                            <br></br>
                             <br></br>
 
-                            <br></br>
-                            <p className="text-left text-light">dont have account! create one:</p>
                             <div class="d-grid gap-2">
-                                <button type="button" className="btn btn-success  btn-block" onClick={() => { history.push("/signup") }}>Sign up</button>
+                                <button type="button" className="btn btn-success  btn-block" disabled={!validateFormFields()} onClick={() => { history.push("/") }} onChange={sendData}>Sign up</button>
                             </div>
-                        </form>
-                    </div>
+
+
+
+
+                        </div>
+                    </form>
+
                 </div>
+
+
+
             </div>
         </div>
+
     );
 }
-
-export default Login;
-
-
 
