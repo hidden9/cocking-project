b746fbe4b1b6b3e87b4946db9b4c16c1cbe640f6
hidden9 <39252771+hidden9@users.noreply.github.com>
2021-07-01 23:50:02 +0430

    dfdf

--- src/signup.js
+++ src/signup.js
@@ -14,7 +14,16 @@ export default function SignUp() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [password_confimation, setPassword_confimation] = useState("");
+
     const [name, setName] = useState("");
+
+
+    let sendData = () => {
+        axios.post('localhost:8000/api/register', { email,name, password, password_confimation })
+            .then(res => console.log('Data send'))
+            .catch(err => console.log(err.data))
+    }
 
 
 
@@ -32,46 +41,50 @@ export default function SignUp() {
     let history = useHistory();
 
     return (
-        <div className = "body bg-dark"  style={{ paddingBottom: "20%" }}>
-        <div className='container  ' style={{ padding: "10%" }}>
-            <h4 className="text-center text-light">Sign up</h4>
-            <br></br>
-            <div className='row h-100 justify-content-center align-items-center'>
+        <div className="body bg-dark" style={{ paddingBottom: "20%" }}>
+            <div className='container  ' style={{ padding: "10%" }}>
+                <h4 className="text-center text-light">Sign up</h4>
+                <br></br>
+                <div className='row h-100 justify-content-center align-items-center'>
 
 
-                <form onSubmit={handleSubmit}>
-                    <div className="form-row ">
-                        <div className="form-group col-lg-12 my-3">
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-row ">
+                            <div className="form-group col-lg-12 my-3">
 
-                            <input type="name" className="form-control" id="inputname4" placeholder="Name" onChange={e => setName(e.target.value)}></input>
+                                <input type="name" className="form-control" id="inputname4" placeholder="Name" onChange={e => setName(e.target.value)}></input>
+                            </div>
+                            <div class="form-group col-lg-12 my-3">
+
+                                <input type="email" className="form-control" id="inputEmail4" placeholder="Email" onChange={e => setEmail(e.target.value)}></input>
+                            </div>
+                            <div class="form-group col-lg-12 my-3">
+
+                                <input type="password" className="form-control" id="inputPassword4" placeholder="Password" ></input>
+                            </div>
+                            <div class="form-group col-lg-12 my-3">
+
+                                <input type="password" className="form-control" id="inputPassword4" placeholder="Confirm Password"  onChange={e => setPassword(e.target.value)}  ></input>
+                            </div>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+
+                            <div class="d-grid gap-2">
+                                <button type="button" className="btn btn-success  btn-block" disabled={!validateFormFields()} onClick={() => { history.push("/") }} onChange={sendData}>Sign up</button>
+                            </div>
+
+
+
+
                         </div>
-                        <div class="form-group col-lg-12 my-3">
+                    </form>
 
-                            <input type="email" className="form-control" id="inputEmail4" placeholder="Email" onChange={e => setEmail(e.target.value)}></input>
-                        </div>
-                        <div class="form-group col-lg-12 my-3">
-
-                            <input type="password" className="form-control" id="inputPassword4" placeholder="Password" onChange={e => setPassword(e.target.value)}></input>
-                        </div>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-
-                        <div class="d-grid gap-2">
-                            <button type="button" className="btn btn-success  btn-block" disabled={!validateFormFields()} onClick={() => { history.push("/") }}>Sign up</button>
-                        </div>
+                </div>
 
 
-
-
-                    </div>
-                </form>
 
             </div>
-
-
-
-        </div>
         </div>
 
     );
